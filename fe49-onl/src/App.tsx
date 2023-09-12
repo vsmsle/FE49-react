8c1a58d4751b8d65579987d429a0a365bb6fea70
vsmsle <shalfeyfo@gmail.com>
2023-09-12 04:36:00 +0300

    btn

--- fe49-onl/src/App.tsx
+++ fe49-onl/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './ui/button/button';
 
 function App() {
+
+  const onClick = () => {
+    console.log(1);
+    console.log(2);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Button onClick={onClick}>OK</Button>
   );
 }
 
